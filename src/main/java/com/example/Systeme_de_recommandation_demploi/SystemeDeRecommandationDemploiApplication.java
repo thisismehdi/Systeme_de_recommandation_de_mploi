36fb822c701e1702c1b52f1c04819f45dddf1660
thisismehdi <mehdifilalipro@gmail.com>
2024-01-11 11:23:54 +0100

    Initial commit

---
+++ src/main/java/com/example/Systeme_de_recommandation_demploi/SystemeDeRecommandationDemploiApplication.java
@@ -0,0 +1,13 @@
+package com.example.Systeme_de_recommandation_demploi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SystemeDeRecommandationDemploiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SystemeDeRecommandationDemploiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Systeme_de_recommandation_demploi/SystemeDeRecommandationDemploiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Systeme_de_recommandation_demploi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SystemeDeRecommandationDemploiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
